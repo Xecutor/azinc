0f846b79563f03f9b122d44d25db0407799f0952
Konstantin Stupnik <konstantin.stupnik@akcp.com>
2017-07-07 16:38:40 +0700

    react deps

--- webpack.release.config.js
+++ webpack.release.config.js
@@ -40,8 +40,8 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
+    /*externals: {
         "react": "React",
         "react-dom": "ReactDOM"
-    },
+    },*/
 };
